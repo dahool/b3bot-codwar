f30a52e16ead433f46302f560dbe1b9856617bd0
Gabriel <gabriel@opensuse.org>
2011-05-06 10:23:25 -0300

    done

--- b3/extplugins/registered.py
+++ b3/extplugins/registered.py
@@ -28,7 +28,9 @@ import b3.events
 #--------------------------------------------------------------------------------------------------
 class RegisteredPlugin(b3.plugin.Plugin):
     requiresConfigFile = False
-    
+   
+    _warn = 15
+
     def onStartup(self):
         self.registerEvent(b3.events.EVT_CLIENT_AUTH)
         
@@ -42,9 +44,10 @@ class RegisteredPlugin(b3.plugin.Plugin):
             return
         
         client = event.client
+        #self.debug("Client %s level is %s" % (client.name, client.maxLevel))
         if client.maxLevel > 0:
             return
-        if client.connections == 1:
+        if client.connections <= 2:
             t = threading.Timer(30, self._client_connected, (client,))
             t.start()
         else:
@@ -55,7 +58,7 @@ class RegisteredPlugin(b3.plugin.Plugin):
             # this requires poweradmin to keep forced
             client.setvar(self, 'paforced', 'spectator')
             self.console.write('forceteam %s %s' % (client.cid, 'spectator'))
-            for i in range(0,15):
+            for i in range(0,self._warn):
                 client.message("^5%s ^7tu id es ^2%s^7. Pedi tu registro en www.codwar.com.ar" % (client.name, client.id))
                 time.sleep(1)
                 self.console.write('forceteam %s %s' % (client.cid, 'spectator'))
@@ -67,27 +70,29 @@ if __name__ == '__main__':
     import time
     
     # first time user
-    user0 = FakeClient(fakeConsole, name="Joe0", exactName="Joe", guid="1234", groupBits=0, team=b3.TEAM_RED)
+    user0 = FakeClient(fakeConsole, name="New User", exactName="Joe", guid="1234", groupBits=0, team=b3.TEAM_RED)
     user0.connections = 0
     
     # second time user
-    user1 = FakeClient(fakeConsole, name="Joe1", exactName="Joe", guid="12384", groupBits=0, team=b3.TEAM_RED)
+    user1 = FakeClient(fakeConsole, name="New User Again", exactName="Joe", guid="12384", groupBits=0, team=b3.TEAM_RED)
     user1.connections = 1
     # registered user
-    user2 = FakeClient(fakeConsole, name="Joe2", exactName="Joe", guid="1235", groupBits=1, team=b3.TEAM_RED)
+    user2 = FakeClient(fakeConsole, name="Registered User", exactName="Joe", guid="1235", groupBits=1, team=b3.TEAM_RED)
     # regular user
-    user2 = FakeClient(fakeConsole, name="Joe2", exactName="Joe", guid="1235", groupBits=4, team=b3.TEAM_RED)
+    user3 = FakeClient(fakeConsole, name="Regular User", exactName="Joe", guid="12365", groupBits=2, team=b3.TEAM_RED)
        
     
     p = RegisteredPlugin(fakeConsole)
+    p._warn = 1
     p.onStartup()
     time.sleep(2)
     
     superadmin.connects(cid=0)
-    time.sleep(2)
+    time.sleep(5)
     user0.connects(cid=1)
-    time.sleep(2)
+    time.sleep(5)
     user1.connects(cid=2)
-    time.sleep(2)
+    time.sleep(5)
     user2.connects(cid=3)
-    time.sleep(2)
+    time.sleep(5)
+    user3.connects(cid=4)
